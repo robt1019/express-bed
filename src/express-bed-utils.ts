28e2c228367c838c0dc20ab18ab5b6228f93c9f6
robt1019 <robert.i.taylor88@gmail.com>
2018-06-28 22:48:42 +0100

    style(): reformat with prettier

--- src/express-bed-utils.ts
+++ src/express-bed-utils.ts
@@ -1,55 +1,53 @@
-
 export class ExpressBedUtils {
-    public static instantiateInjectables(injectables?: any[]): any[] {
-        if (!injectables) {
-            return [];
-        }
-        return this.resolveInjectables(injectables);
+  public static instantiateInjectables(injectables?: any[]): any[] {
+    if (!injectables) {
+      return [];
+    }
+    return this.resolveInjectables(injectables);
+  }
+
+  public static instantiateRoutesAndInjectables(testConfig: {
+    routes?: any[];
+    injectables?: any[];
+  }): { instantiatedRoutes: any[]; instantiatedInjectables: any[] } {
+    const routes: any[] = [];
+    let injectablesToReturn: any[] = [];
+
+    if (testConfig.routes) {
+      testConfig.routes.forEach((route: any) => {
+        injectablesToReturn = this.resolveInjectables(testConfig.injectables);
+        routes.push(new route(...injectablesToReturn));
+      });
     }
 
-    public static instantiateRoutesAndInjectables(testConfig: {
-        routes?: any[];
-        injectables?: any[];
-    }): { instantiatedRoutes: any[]; instantiatedInjectables: any[] } {
-        const routes: any[] = [];
-        let injectablesToReturn: any[] = []
+    return {
+      instantiatedRoutes: routes,
+      instantiatedInjectables: injectablesToReturn
+    };
+  }
 
-        if (testConfig.routes) {
-            testConfig.routes.forEach((route: any) => {
-                injectablesToReturn = this.resolveInjectables(testConfig.injectables);
-                routes.push(new route(...injectablesToReturn));
-            });
+  private static resolveInjectables(injectables?: any[]): any[] {
+    const constructorArgs: any[] = [];
+
+    if (injectables) {
+      injectables.forEach((injectable: any) => {
+        let injectableArg: any;
+
+        if (injectable.useValue) {
+          injectableArg = injectable.useValue;
+        } else {
+          if (injectable.injectables) {
+            injectableArg = new injectable.inject(
+              ...this.resolveInjectables(injectable.injectables)
+            );
+          } else {
+            injectableArg = new injectable();
+          }
         }
 
-        return {
-            instantiatedRoutes: routes,
-            instantiatedInjectables: injectablesToReturn,
-        };
+        constructorArgs.push(injectableArg);
+      });
     }
-
-    private static resolveInjectables(injectables?: any[]): any[] {
-
-        const constructorArgs: any[] = [];
-
-        if (injectables) {
-            injectables.forEach((injectable: any) => {
-                let injectableArg: any;
-
-                if (injectable.useValue) {
-                    injectableArg = injectable.useValue;
-                } else {
-                    if (injectable.injectables){
-                        injectableArg =
-                            new injectable.inject(...(this.resolveInjectables(injectable.injectables)));
-                    }
-                    else {
-                        injectableArg = new injectable();
-                    }
-                }
-
-                constructorArgs.push(injectableArg);
-            });
-        }
-        return constructorArgs;
-    }
+    return constructorArgs;
+  }
 }
--- src/express-bed.ts
+++ src/express-bed.ts
@@ -1,55 +1,51 @@
-import {ExpressBedUtils} from './express-bed-utils';
-import {Router} from 'express';
+import { ExpressBedUtils } from './express-bed-utils';
+import { Router } from 'express';
 import express from 'express';
 
 export interface ExpressTestBed {
-    app: express.Application;
-    get(token: any): any;
+  app: express.Application;
+  get(token: any): any;
 }
 
 export interface BaseRoute {
-   create(router: Router): void
+  create(router: Router): void;
 }
 
 export class ExpressBed {
+  public static configureTestingModule(testConfig: {
+    routes?: any[];
+    injectables?: any[];
+  }): ExpressTestBed {
+    let instantiatedRoutes: any[] = [];
+    let instantiatedInjectables: any = {};
 
-    public static configureTestingModule(
-        testConfig: {
-            routes?: any[];
-            injectables?: any[];
-        },
-    ): ExpressTestBed {
+    const app = express();
 
-        let instantiatedRoutes: any[] = [];
-        let instantiatedInjectables: any = {};
-
-        const app = express();
-
-        if (testConfig.routes) {
-            const routesAndInjectables = ExpressBedUtils.instantiateRoutesAndInjectables(
-                testConfig,
-            );
-            instantiatedRoutes = routesAndInjectables.instantiatedRoutes;
-            instantiatedInjectables = routesAndInjectables.instantiatedInjectables;
-        } else {
-            instantiatedInjectables = ExpressBedUtils.instantiateInjectables(
-                testConfig.injectables,
-            );
-        }
-
-        instantiatedRoutes.forEach((route) => {
-            route.create(app);
-        });
-
-        return {
-            app,
-            get(token: any): any {
-                return instantiatedInjectables
-                    .find((injectable: any) => {
-                        return token.toString()
-                            .includes(`function ${injectable.constructor.name}(`);
-                    });
-            },
-        };
+    if (testConfig.routes) {
+      const routesAndInjectables = ExpressBedUtils.instantiateRoutesAndInjectables(
+        testConfig
+      );
+      instantiatedRoutes = routesAndInjectables.instantiatedRoutes;
+      instantiatedInjectables = routesAndInjectables.instantiatedInjectables;
+    } else {
+      instantiatedInjectables = ExpressBedUtils.instantiateInjectables(
+        testConfig.injectables
+      );
     }
+
+    instantiatedRoutes.forEach(route => {
+      route.create(app);
+    });
+
+    return {
+      app,
+      get(token: any): any {
+        return instantiatedInjectables.find((injectable: any) => {
+          return token
+            .toString()
+            .includes(`function ${injectable.constructor.name}(`);
+        });
+      }
+    };
+  }
 }
